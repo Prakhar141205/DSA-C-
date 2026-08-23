e0a2bdc0b564cf03bed354d6c68df01348d879bb
Prakhar141205 <prakhar141205@gmail.com>
2026-08-23 23:50:04 +0530

    add questions

---
+++ Aptitude/1927. sum game.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.length();
+
+        int leftKnownSum = 0;
+        int rightKnownSum = 0;
+
+        int leftQnMarkCount = 0 ;
+        int rightQnMarkCount = 0;
+
+        for(int i=0; i<n; i++)  {
+            if(num[i] == '?') {
+                if(i < n/2) {
+                    leftQnMarkCount++;
+                }else {
+                    rightQnMarkCount++;
+                }
+
+            }else {
+                if(i < n/2 ) {
+                    leftKnownSum += num[i] - '0';
+                }else {
+                    rightKnownSum += num[i] - '0';
+                }
+            }
+            
+        }
+
+
+        int totalQnMarks = leftQnMarkCount + rightQnMarkCount ;
+
+        if(totalQnMarks & 1) return true;
+
+        int left = 2 * leftKnownSum + 9 * leftQnMarkCount ;
+
+        int right = 2 * rightKnownSum + 9 * rightQnMarkCount;
+
+        
+        return left == right ? false : true ;
+        
+    }
+};
